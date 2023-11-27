5ea504250885030f0778028f82fcbd450aeed317
Petrenko Yevhen <jenyamolodec@gmail.com>
2023-11-28 01:15:25 +0300

    finish

--- src/mane/java/ua/hillel/Petrenko/lessons/lessons2/main2.java
+++ src/mane/java/ua/hillel/Petrenko/lessons/lessons2/main2.java
@@ -5,5 +5,14 @@ public class main2 {
         double n = 51.17890;
         double w = 1.82632;
         char grad = '\u00b0';
-        System.out.println("Stonehenge");
+        System.out.println(" Координаты Stonehenge: N=" + n + grad + "W=" + w + grad);
+        int nN = 51;
+        int wW = 1;
+        double norse = 10.734;
+        double west = 49.579;
+        char caps = '\u2019';
+        System.out.println(" Координаты Stonehenge: N=" + nN + grad + norse + caps);
+        System.out.println(" Координаты Stonehenge: W=" + wW + grad + west + caps);
+
+    }
 }
