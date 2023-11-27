057cbc6a51ad157a584ced28a478a9dd6ccfe0b7
Petrenko Yevhen <jenyamolodec@gmail.com>
2023-11-27 21:25:35 +0300

    add all files

---
+++ src/mane/java/ua/hillel/Petrenko/lessons/lessons1/main.java
@@ -0,0 +1,8 @@
+package ua.hillel.Petrenko.lessons.lessons1;
+
+public class main {
+    public static void main(String[] args) {
+            System.out.println("HELLO World");
+
+    }
+}
---
+++ src/mane/java/ua/hillel/Petrenko/lessons/lessons2/main2.java
@@ -0,0 +1,7 @@
+package ua.hillel.Petrenko.lessons.lessons2;
+
+public class main2 {
+    public static void main(String[] args) {
+
+    }
+}
