b4d3b1ba5767f3dc3cd97c16c6789e2c0ae25e92
Petrenko Yevhen <jenyamolodec@gmail.com>
2023-11-28 00:56:39 +0300

    Stonehenge

--- src/mane/java/ua/hillel/Petrenko/lessons/lessons2/main2.java
+++ src/mane/java/ua/hillel/Petrenko/lessons/lessons2/main2.java
@@ -2,6 +2,8 @@ package ua.hillel.Petrenko.lessons.lessons2;
 
 public class main2 {
     public static void main(String[] args) {
-
-    }
+        double n = 51.17890;
+        double w = 1.82632;
+        char grad = '\u00b0';
+        System.out.println("Stonehenge");
 }
