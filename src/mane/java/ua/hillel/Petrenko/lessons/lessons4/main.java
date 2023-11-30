c0063ae6c413356b995265f539e10e0fd9bf889e
Yevhen Petrenko <jenyamolodec@gmail>
2023-11-30 22:08:45 +0300

    home4error

---
+++ src/mane/java/ua/hillel/Petrenko/lessons/lessons4/main.java
@@ -0,0 +1,14 @@
+package ua.hillel.Petrenko.lessons.lessons4;
+
+public class main {
+    public static void main(String[] args) {
+            int sideA = 4;
+            int sideB = 3;
+            int sideC = 5;
+        int volume = sideA * sideB * sideC;
+           System.out.println("Îבתול= " + volume);
+           int length = 4 * (sideA + sideB + sideC);
+           System.out.println("Ñףללא= " + length);
+        }
+    }
+
---
+++ src/mane/java/ua/hillel/Petrenko/lessons/lessons4/main2.java
@@ -0,0 +1,14 @@
+package ua.hillel.Petrenko.lessons.lessons4;
+
+public class main2 {
+    public static void main(String[] args) {
+//   Сила династии Ли
+              int liVoin = 13;
+              int liArcher = 24;
+              int liRider= 46;
+//  Сила династии Минь
+              int MiVoin = 9;
+              int MiArcher = 35;
+              int MiRider= 12;
+    }
+}
